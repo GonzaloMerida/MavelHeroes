a71ab2aed38886108532808fb9f315d18a24f799
Gonzalo <gonzalomerida@hotmail.com>
2023-12-14 11:15:19 +0100

    Initial commit

---
+++ app/src/main/java/com/santosgo/mavelheroes/Hero.kt
@@ -0,0 +1,10 @@
+package com.santosgo.mavelheroes
+
+data class Hero(
+    private val name : String = "",
+    private val power : Int = 0,
+    private val intelligence : Int = 0,
+    private val photo : String = "",
+    private val description : String = ""
+)
+
