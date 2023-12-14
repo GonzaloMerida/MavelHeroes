f863d545a13472b8d07595a8b47f60cfd77855aa
Gonzalo <gonzalomerida@hotmail.com>
2023-12-14 12:46:15 +0100

    a medias

--- app/src/main/java/com/santosgo/mavelheroes/Hero.kt
+++ app/src/main/java/com/santosgo/mavelheroes/Hero.kt
@@ -1,10 +1,11 @@
 package com.santosgo.mavelheroes
 
 data class Hero(
-    private val name : String = "",
-    private val power : Int = 0,
-    private val intelligence : Int = 0,
-    private val photo : String = "",
-    private val description : String = ""
+    val name : String = "",
+    val power : Int = 0,
+    val intelligence : Int = 0,
+    val photo : String = "",
+    val description : String = ""
 )
+
 
--- app/src/main/java/com/santosgo/mavelheroes/HeroListFragment.kt
+++ app/src/main/java/com/santosgo/mavelheroes/HeroListFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.santosgo.mavelheroes.data.Datasource
 import com.santosgo.mavelheroes.databinding.FragmentHeroListBinding
 
 
@@ -27,8 +28,18 @@ class HeroListFragment : Fragment() {
         // Inflate the layout for this fragment
         _binding = FragmentHeroListBinding.inflate(inflater,container,false)
 
+        val heroes = Datasource.getHeroList()
+
+
         //texto inicial
         binding.textView.text = getString(R.string.hero_list)
+        //para la otra forma
+        //binding.textView.text = getString(R.string.hero_list) + "\n\n"
+        heroes.forEach {
+            binding.textView.append("\nHeroe: ${it.name}")
+            //otra forma
+            //binding.textView.text =P binding.textView.toString() + "Heroe: ${hero.name}\n"
+        }
 
         return binding.root
     }
